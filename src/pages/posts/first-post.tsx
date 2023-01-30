bc41418973054c8b71638aca07a89a9842989a26
yuba.khadka <yuba.khadka@fifthdomain.com.au>
2023-01-31 10:25:06 +1100

    add md files

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,20 +1,14 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../../components/layout';
-import utilStyles from '@/styles/utils.module.css';
+import Layout, { siteTitle } from '@/components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <Layout home>
+      <Layout home={false}>
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
-          <p>
-            I am a passionate Full Stack Developer. I eat Nodejs, React, Graphql
-            and AWS.{' '}
-          </p>
-        </section>
+       
       </Layout>
     </>
   );
